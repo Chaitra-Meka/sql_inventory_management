e7dd40eb5da5231740c79f764d1c433fa81966d5
Chaitra Meka <chaitra.meka@resolence.com>
2026-09-25 18:02:18 +0530

    Create inventory management project

--- config/database.py
+++ config/database.py
@@ -1,12 +1,16 @@
+import os
 import psycopg
-
-
+from dotenv import load_dotenv
+ 
+load_dotenv()
+ 
+ 
 DB_CONFIG = {
-    "host": "localhost",
-    "port": 5432,
-    "dbname": "inventory_db",
-    "user": "postgres",
-    "password": "2525"
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD")
 }
 
 
